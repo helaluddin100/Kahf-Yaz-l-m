49e66694080649259a4802f18dd427f787134025
Helal Uddin <freelancerhelaluddin@gmail.com>
2024-10-10 02:14:14 +0600

    remove unused package

--- component/Layout/Header.js
+++ component/Layout/Header.js
@@ -1,6 +1,26 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 function Header() {
-  return <>Header</>;
+  return (
+  <header>
+    <div className="navigation">
+      <div className="container">
+        <div className="header-wrapper">
+
+          <div className="logo">
+            <Link href="/">
+              <a>
+                <img src="assets/img/logo.png" alt="Logo" />
+                <p>devlinks</p>
+              </a>
+             
+            </Link>
+          </div>
+        </div>
+        </div>
+    </div>
+  </header>
+  
+  );
 }
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -11,28 +11,11 @@ function Home() {
           In Non-Fungible Token Development Services, We Are the Experts – NFT
           Constructer
         </title>
-        <meta property="og:site_name" content="NFT CONSTRUCTER"></meta>
-        <meta
-          name="google-site-verification"
-          content="qLqpdaJEdfR47ipB9hZYNBRaViXucx7nOp3B5VCNLb0"
-        />
-        <meta property="og:type" content="website"></meta>
-        <meta property="og:url" content="https://www.nftconstructer.com" />
-        <link rel="canonical" href="https://nftconstructer.com/ " />
+
         <meta
           name="description"
           content="NFT Constructer offers highly professional website design and development services with affordable pricing plans specialized for NFT DeFi Blockchain and Web3 based projects corporate clients and startups."
         ></meta>
-        <meta
-          name="og:description"
-          content="NFT Constructer offers highly professional website design and development services with affordable pricing plans specialized for NFT DeFi Blockchain and Web3 based projects corporate clients and startups."
-        ></meta>
-        <meta property="og:type" content="article" />
-        <meta
-          property="og:title"
-          content="In Non-Fungible Token Development Services, We Are the Experts – NFT Constructer "
-        />
-        <meta property="og:image" content="assets/img/meta/1.jpg" />
         <meta property="image" content="assets/img/meta/1.jpg" />
         <meta
           name="keywords"
@@ -40,12 +23,6 @@ function Home() {
         ></meta>
         <link rel="icon" type="image" href="/favicon.png"></link>
       </Head>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-MLNPCQK"
-          className="tag-man"
-        ></iframe>
-      </noscript>
 
       <div>
         <h2>Hello BOdy</h2>
